f4c42efd4789b53fac2771ec05fab9af2c7f46e1
Cyntia <cyntia.aparecida@hotmail.com>
2018-09-14 14:15:55 -0300

    first commit

---
+++ app/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+    return <div>I'm a cool girl!</div>;
+};
+
+ReactDOM.render(<Index />,document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+const path = require('path')
+
+module.exports = {
+    entry: "./app/App.js",
+    output: {
+        filename: "public/bundle.js"
+  },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            query: {
+                presets: ['react']
+            }
+          }
+        }
+      ]
+    }
+  };
